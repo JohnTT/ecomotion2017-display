63a6606037000d858a26c2381efc4ed94b4cd2b9
JohnChen <jmc_tt@live.com>
2016-12-29 20:45:23 -0600

    Initial Commit

---
+++ Debug/Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.d
@@ -0,0 +1,101 @@
+Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.o: \
+ ../Drivers/STM32F3xx_HAL_Driver/Src/stm32f3xx_hal_uart.c \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Inc/stm32f3xx_hal_conf.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Inc/main.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xe.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cm4.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cmSimd.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h \
+ C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Inc/stm32f3xx_hal_conf.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Inc/main.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xe.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cm4.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Include/core_cmSimd.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_adc_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi_ex.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart.h:
+
+C:/Users/jmc_t/Documents/Git/stm32cubemx_f303re/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_uart_ex.h:
