e6b026fb92f2ef34513c5c686ac2c16308ce70bb
Liam Sparling <mrbiam@users.noreply.github.com>
2017-03-21 10:35:45 -0500

    Added display.h for PFPs

---
+++ Inc/display.h
@@ -0,0 +1,37 @@
+/*
+ * display.h
+ *
+ *  Created on: Mar 21, 2017
+ *      Author: Constellations
+ */
+
+#ifndef DISPLAY_H_
+#define DISPLAY_H_
+
+void init();
+void waitTCBusy();
+int readResponse(int Le);
+void readStrResponse();
+void clearImageBuffer();
+void setAllBlack();
+void setAllWhite();
+void setXY(dispColour colour, int x, int y);
+void drawRectangle(dispColour colour, int X1, int Y1, int X2, int Y2);
+void clearRectangle(int X1, int Y1, int X2, int Y2);
+void drawDiagonal(dispColour colour, int X1, int Y1, int X2, int Y2, int dx, int dy);
+int drawCharacter(dispColour colour, int x, int y, int pt, char c);
+int drawString(dispColour colour, int x, int y, int pt, int sp, char s[], int size);
+void batteryImage(int x, int y, int size, int fontSize, int fontSpacing, int percent);
+void testDraw();
+void delay(int x);
+// Functions defined for the TCM 441-230
+void uploadImageBuffer();
+void resetDataPointer();
+void displayUpdate();
+void getDeviceInfo();
+void getDeviceID();
+void getSystemInfo();
+void getSystemVersionCode();
+void readSensorData();
+
+#endif /* DISPLAY_H_ */
--- Src/main.c
+++ Src/main.c
@@ -82,31 +82,6 @@ static void MX_SPI1_Init(void);
 /* USER CODE BEGIN PFP */
 /* Private function prototypes -----------------------------------------------*/
 static void setCANbitRate(uint16_t bitRate, uint16_t periphClock, CAN_HandleTypeDef* theHcan);
-void init();
-void waitTCBusy();
-int readResponse(int Le);
-void readStrResponse();
-void clearImageBuffer();
-void setAllBlack();
-void setAllWhite();
-void setXY(dispColour colour, int x, int y);
-void drawRectangle(dispColour colour, int X1, int Y1, int X2, int Y2);
-void clearRectangle(int X1, int Y1, int X2, int Y2);
-void drawDiagonal(dispColour colour, int X1, int Y1, int X2, int Y2, int dx, int dy);
-int drawCharacter(dispColour colour, int x, int y, int pt, char c);
-int drawString(dispColour colour, int x, int y, int pt, int sp, char s[], int size);
-void batteryImage(int x, int y, int size, int fontSize, int fontSpacing, int percent);
-void testDraw();
-void delay(int x);
-// Functions defined for the TCM 441-230
-void uploadImageBuffer();
-void resetDataPointer();
-void displayUpdate();
-void getDeviceInfo();
-void getDeviceID();
-void getSystemInfo();
-void getSystemVersionCode();
-void readSensorData();
 /* USER CODE END PFP */
 
 /* USER CODE BEGIN 0 */
